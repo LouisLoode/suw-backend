b7a6d5d55ab7e54536f7dfd6ef420d349a430dde
Louis DEBRAINE <louisdebraine@gmail.com>
2017-06-07 13:51:07 +0200

    Fix package.json

--- config/config.js
+++ config/config.js
@@ -2,6 +2,13 @@ if (process.env.NODE_ENV === undefined) {
     process.env.NODE_ENV = 'development';
 }
 
+console.log("process.env.NODE_ENV : "+process.env.NODE_ENV);
+console.log("process.env.API_HOST : "+process.env.API_HOST);
+console.log("process.env.API_PORT : "+process.env.API_PORT);
+console.log("process.env.API_URL : "+process.env.API_URL);
+console.log("process.env.MONGODB_URI : "+process.env.MONGODB_URI);
+console.log("process.env.PRIVATE_KEY : "+process.env.PRIVATE_KEY);
+
 module.exports = {
     env: process.env.NODE_ENV || 'development',
     api: {
