91916600f14190cdac77c78a514be5ed2e63971d
Louis DEBRAINE <louisdebraine@gmail.com>
2017-06-07 13:44:01 +0200

    Fix package.json

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,8 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 module.exports = function(app, config, server) {
-	server.listen(config.api.port)
+	console.log("config.api.port : "+config.api.port);
+	server.listen(config.api.port || 3000)
   app.use(cors());
   // setup the logger
   app.use(morgan(':method :url :status :response-time ms - :res[content-length]'))
