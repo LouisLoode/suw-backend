2636397f83853c6b6b66b6aefe193f60b7a9883c
Louis DEBRAINE <louisdebraine@gmail.com>
2017-06-07 13:13:22 +0200

    Update heroku deploy

--- node-app/config/middlewares.js
+++
@@ -1,9 +0,0 @@
-const morgan = require('morgan');
-const cors = require('cors');
-
-module.exports = function(app, config, server) {
-	server.listen(config.api.port)
-  app.use(cors());
-  // setup the logger
-  app.use(morgan(':method :url :status :response-time ms - :res[content-length]'))
-};
